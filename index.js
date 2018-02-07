ed19ef1ee0a5cd11952031087d3e0ddb1215c275
Amit <amit@cocycles.com>
2018-02-07 18:05:51 +0200

    support types

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const precinct = require('precinct');
 
 module.exports = function(src, options = {}) {
   const { script, styles } = compiler.parseComponent(src, { pad: 'line' });
-  const scriptDependencies  = script ? precinct(script.content, { es6: { mixedImports : true } } ).map(dep => ({ type: 'script', dep })) : [];
-  const styleDependencies  = script ? flatten(styles.map(style => precinct(style.content, { type: style.lang || 'scss' } ).map(dep => ({ type: style.lang || 'scss' , dep })))): [];
+  const scriptDependencies  = script ? precinct(script.content, { es6: { mixedImports : true } } ).map(dep => ({ isScript: true, dep })) : [];
+  const styleDependencies  = script ? flatten(styles.map(style => precinct(style.content, { type: style.lang || 'scss' } ).map(dep => ({ isScript: false , dep })))): [];
   return scriptDependencies.concat(styleDependencies);
 };
