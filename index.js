b410af593aab8c5cf6198627bf775c236cf9e1b3
Amit <amit@cocycles.com>
2018-02-07 23:13:15 +0200

    fixed import

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const compiler = require('vue-template-compiler');
 const flatten = require('lodash.flatten');
-const precinct = require('precinct');
-
 
 module.exports = function(src, options = {}) {
+  const precinct = require('precinct');
   const { script, styles } = compiler.parseComponent(src, { pad: 'line' });
   const scriptDependencies  = script ? precinct(script.content, { es6: { mixedImports : true } } ).map(dep => ({ isScript: true, dep })) : [];
   const styleDependencies  = script ? flatten(styles.map(style => precinct(style.content, { type: style.lang || 'scss' } ).map(dep => ({ isScript: false , dep })))): [];
