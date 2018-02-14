59f45cddd8889fbb67e36914558da44caedc857f
Amit <amit@cocycles.com>
2018-02-14 13:13:28 +0200

    support vue

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const flatten = require('lodash.flatten');
 
 module.exports = function(src, options = {}) {
   const precinct = require('precinct');
+  options.useContent = true;
+  options.es6 = { mixedImports : true };
   const { script, styles } = compiler.parseComponent(src, { pad: 'line' });
-  const scriptDependencies  = script ? precinct(script.content, { es6: { mixedImports : true } } ).map(dep => ({ isScript: true, dep })) : [];
+  const scriptDependencies  = script ? precinct(script.content, options ).map(dep => ({ isScript: true, dep })) : [];
   const styleDependencies  = script ? flatten(styles.map(style => precinct(style.content, { type: style.lang || 'scss' } ).map(dep => ({ isScript: false , dep })))): [];
   return scriptDependencies.concat(styleDependencies);
 };
